876af1b67ff54385145f6c5dc97c4de9bb6840c4
Scot MacDonald <scotmacdonald@hotmail.com>
2025-05-16 13:38:30 +0200

    time component footer

--- src/Footer/Component.tsx
+++ src/Footer/Component.tsx
@@ -7,6 +7,7 @@ import type { Footer } from '@/payload-types'
 // import { ThemeSelector } from '@/providers/Theme/ThemeSelector'
 import { CMSLink } from '@/components/Link'
 // import { Logo } from '@/components/Logo/Logo'
+import Time from '@/components/Time'
 
 export async function Footer() {
   const footerData: Footer = await getCachedGlobal('footer', 1)()
@@ -14,16 +15,18 @@ export async function Footer() {
   const navItems = footerData?.navItems || []
 
   return (
-    <footer className="mt-auto border-t border-border bg-black dark:bg-card text-white">
-      <div className="px-4 py-8 gap-8 flex flex-col md:flex-row md:justify-between">
-        <Link className="flex items-center" href="/">
+    <footer className="mt-auto dark:bg-card ">
+      <div className="w-full p-5 gap-8 flex flex-col md:flex-row md:justify-between">
+        <Link className="flex items-center text-sm lg:w-[353px]" href="/">
           {/* <Logo /> */}
+          ©2025 PERMANENT Services GmbH
         </Link>
-
+        <Time />
         <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
-          <nav className="flex flex-col md:flex-row gap-4">
+          {/* <ThemeSelector /> */}
+          <nav className="flex flex-col md:flex-row gap-4 lg:w-[353px]">
             {navItems.map(({ link }, i) => {
-              return <CMSLink className="text-white" key={i} {...link} />
+              return <CMSLink className="dark:bg-card  text-sm" key={i} {...link} />
             })}
           </nav>
         </div>
